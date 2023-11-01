fbcb0bef802f257c8ecdebb7c2d12dab3eb82108
AshishAgam <ashishagam6@gmail.com>
2023-11-01 16:15:49 +0530

    add code

--- hello.py
+++ hello.py
@@ -1,1 +1,33 @@
-print("hello world")
+'''print("hello world")
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=a+b
+print("sum is",c)
+#receving input
+name = input("What is your name: ")
+print("Hello "+name)
+#Type conversion
+#in this example string conver in to integer
+birt_year=input("Enter your birt year: ")
+age = 2023-int(birt_year)
+print(age)
+
+first = float(input ("first:"))
+second = float(input("second:"))
+sum=first+second
+print("addition is", sum)
+#string operations
+course="Python for Beginners"
+print(course.upper())
+print(course.lower())
+print(course.find('y'))
+print(course.replace('for','4'))
+print('Python' in course)
+
+print(10//3)'''
+
+#logical operators
+mrp=5
+print(mrp<10 and mrp>3)
+print(mrp<6 or mrp>6)
+print(not mrp>4)
