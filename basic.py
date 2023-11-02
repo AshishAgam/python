e827a427fa806b656a87e671116cbb066f4fd7fe
AshishAgam <ashishagam6@gmail.com>
2023-11-02 11:34:41 +0530

    add changes

---
+++ basic.py
@@ -0,0 +1,44 @@
+'''ex=1.23+2.80
+print(round(ex,2))
+#string 
+ex1="apricots"
+print(ex1[:3])
+print(ex1[2:5])
+print(ex1[4:])
+
+connected="r2"+"_"+"d2"
+print(connected)
+print(connected[3])
+print(connected[1:4])
+
+unchanged="forrest gump"
+slice=unchanged[6:]
+print(slice)
+print(unchanged[10])
+
+#type() Function
+ex2=True
+ex3=10
+ex4=45.36
+ex5= 'a'
+ex6="ASDF"
+print(type(ex2))
+print(type(ex3))
+print(type(ex4))
+print(type(ex5))
+print(type(ex6))
+
+#escape sequences
+print("ashish\tkuldeep")
+print("ashish\nkuldeep")
+print("ashish kuldeep\\.")
+print('"when I said\' Immediatly,\' I ment today!" said king soul"')
+print("\"Do or do not there is no try.\"")
+
+print("*******\n *****\n  ***\n   *")
+
+#input() Function
+name=input("What is your name:")
+age=input("Enter your age:")
+print("Your name is "+name+".Your name is "+age+".")'''
+
