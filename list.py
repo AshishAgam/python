9cfec0e4168959e6fce1b061a20a282183183a64
AshishAgam <ashishagam6@gmail.com>
2023-11-03 18:00:13 +0530

    string

---
+++ list.py
@@ -0,0 +1,40 @@
+'''print(list("asdf"))
+
+#in and not in keyword in the list
+check_list=[1,2,3,4]
+print(1 in check_list)
+
+check_list=['a','b','c']
+print('a' not in check_list)
+
+mixed = [10, 4.97, True, "mountain", [9, 8, 7]]
+print(type(mixed))
+
+li_str = list("cheese")
+print("e" in li_str)
+print("a" not in li_str)
+
+lst=["Ashish","Kuldeep","Aniket"]
+print(lst[1])
+
+index_example=[[1,2,3],[4,5,6],[7,8,9]]
+print(index_example[2][1])
+print(index_example[2])
+
+mixed = [10, 4.97, True, "mountain", [9, 8, 7]]
+print(mixed[1]+2)
+
+#slicing 
+lst=[1,2,3,4,5,6,7,8,9]
+print(lst[:4])
+print(lst[2:7])
+print(lst[5:])
+print(lst[1:8:2])'''
+
+#reassigning value
+example=[1,2,3,4,56,7,8,99,88,10,]
+print(example)
+example[3]=12
+print(example)
+example[0:3]=[10,110,22]
+print(example)
---
+++ str_opetation.py
@@ -0,0 +1,29 @@
+'''print("python".upper())
+print("BACKEND".lower())
+print("python".islower())
+print("BACKEND".isupper())
+
+b="python use for backend development"
+for i in b:
+    print(i.isalpha()
+
+print("python use for backend development".isalpha())
+print("python use for backend development".isalnum())
+print("python use for backend development".isspace())
+print("Python Use For Backend Development".istitle())
+print("python use for backend development".startswith("p"))
+print("python use for backend development".endswith("t"))
+
+print("".join(["one","two","three"]))
+print(" ".join(["one","two","three"]))
+print(",".join(["one","two","three"]))
+print(" ,".join(["one","two","three"]))
+print("....".join(["one","two","three"]))'''
+
+a="12"
+b=(a.isalnum())     
+print(b)
+
+
+a="one two three"
+print(a.split())
